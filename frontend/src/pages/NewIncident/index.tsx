865e948c63b584b3e521212f84248dae330f3e59
azagatti <andre.zagatti1@gmail.com>
2020-03-25 15:29:16 -0300

    feat: finished first version without api

---
+++ frontend/src/pages/NewIncident/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg";
+
+import "./styles.css";
+
+const NewIncident: React.FC = () => {
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhadamente para encontrar um herói para resolver
+            isso.
+          </p>
+
+          <Link className="backlink" to="/profile">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para home
+          </Link>
+        </section>
+        <form>
+          <input placeholder="Título do caso" />
+          <textarea placeholder="Descrição" />
+
+          <input placeholder="Valor em reais" />
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default NewIncident;
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Logon from "./pages/Logon";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import NewIncident from "./pages/NewIncident";
 
 const Routes: React.FC = () => {
   return (
@@ -11,7 +12,9 @@ const Routes: React.FC = () => {
       <Switch>
         <Route path="/" exact component={Logon} />
         <Route path="/register" component={Register} />
+
         <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
